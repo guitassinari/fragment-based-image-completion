c2c1b09ca4ac1564d89b074e1f296de7e0a6293b
DenysonJ <denysonjurgen@gmail.com>
2017-12-19 00:03:58 -0200

    confidence map
    First attempt to finish confidence map

--- helpers/confidence.js
+++ helpers/confidence.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {std, mean} from 'mathjs'
+import pdf from 'distributions-normal-pdf'
 
 const ERRO = -1;
 
@@ -24,22 +25,51 @@ function neighborhood(M, ii, jj) {
         if (ii < (M.length - 1)) {
             if (jj > 0) {
                 if (jj < (M[ii].length - 1)) {
-                    return [[M[ii - 1][jj-1], M[ii-1][jj], M[ii-1][jj+1]], [M[ii][jj-1], M[ii][jj], M[ii][jj+1]], [M[ii+1][jj-1], M[ii+1][jj], M[ii+1][jj+1]]];
+                    return [[M[ii - 1][jj-1], M[ii-1][jj], M[ii-1][jj+1]], [M[ii][jj-1], M[ii][jj], M[ii][jj+1]], [M[ii+1][jj-1], M[ii+1][jj], M[ii+1][jj+1]]]; // 1 1
                 } 
                 else {
-                    return [M[ii - 1][jj-1], M[ii-1][jj], M[ii][jj-1], M[ii][jj], M[ii+1][jj-1], M[ii+1][jj]];
+                    return [[M[ii - 1][jj-1], M[ii-1][jj]], [M[ii][jj-1], M[ii][jj]], [M[ii+1][jj-1], M[ii+1][jj]]]; // 1 2
                 }
             } 
             else {
-            
+               return [[M[ii-1][jj], M[ii-1][jj+1]], [M[ii][jj], M[ii][j+1]], [M[ii+1][jj], M[ii+1][jj+1]]]; // 1 0         
             }
         } 
         else {
-
+            if (jj > 0) {
+                if (jj < (M[ii].length - 1)) {
+                    return [[M[ii - 1][jj-1], M[ii-1][jj], M[ii-1][jj+1]], [M[ii][jj-1], M[ii][jj], M[ii][jj+1]]]; // 2 1
+                } 
+                else {
+                    return [[M[ii - 1][jj-1], M[ii-1][jj]], [M[ii][jj-1], M[ii][jj]]]; // 2 2
+                }
+            }
+            else {
+               return [[M[ii-1][jj], M[ii-1][jj+1]], [M[ii][jj], M[ii][j+1]]]; // 2 0         
+            }
         }
     } 
     else {
+        if (jj > 0) {
+            if (jj < (M[ii].length - 1)) {
+                return [[M[ii][jj-1], M[ii][jj], M[ii][jj+1]], [M[ii+1][jj-1], M[ii+1][jj], M[ii+1][jj+1]]]; // 0 1
+            } 
+            else {
+                return [[M[ii][jj-1], M[ii][jj]], [M[ii+1][jj-1], M[ii+1][jj]]]; // 0 2
+            }
+        }
+        else {
+            return [[M[ii][jj], M[ii][j+1]], [M[ii+1][jj], M[ii+1][jj+1]]]; // 0 0         
+        }
+    }
+}
 
+function inverse(number) {
+    if (number == 0) {
+        return 1;
+    } 
+    else {
+        return 0;
     }
 }
 
@@ -51,13 +81,20 @@ function beta(alphaMatte, i, j) {
         let N = neighborhood(alphaMatte, i, j);
         let mu = mean(N);
         let sdeviation = std(N);
+        let sum = 0;
+        
+        for (let count_i = 0; count_i < N.length; count_i++) {
+            for (let count_j = 0; count_j < N[0].length; count_j++) {
+                sum += (pdf(N[count_i][count_j], mu, sdeviation) * inverse(N[count_i][count_j]));
+            }
+        }
     }
 }
 
 export function confidenceMap(alphaMatte) {
     let confidence = createArray(alphaMatte.length, alphaMatte[0].length);
-    for (var j = 0; j < alphaMatte.length; j++) {
-        for (var i = 0; i < alphaMatte[j].length; i++) {
+    for (let j = 0; j < alphaMatte.length; j++) {
+        for (let i = 0; i < alphaMatte[j].length; i++) {
             confidence[i][j] = beta(alphaMatte, i, j);
         }
     }
